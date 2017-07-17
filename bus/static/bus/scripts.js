6ce571d5958aa85de3e459dfe2f6bd66f44c8fba
connormross <connor.ross@ucdconnect.ie>
2017-07-17 14:21:17 +0100

    added code to check if input fields are empty and wrote short get request for list of routes

--- bus/static/bus/scripts.js
+++ bus/static/bus/scripts.js
@@ -33,24 +33,56 @@ $(document).ready(function(){
 });
 
 //checks whether the form fields are empty and displays an error message if they are
-$(document).ready(function(){
-    $("#submitBtn").click(function(){
-        var isValid = true;
-        $(".form-control").each(function() {
-            if ( $(this).val() === '' )
-                isValid = false;
-        });
-        if (!isValid)
-            alert("One or more input fields have been left empty. Please make sure to fill all of them :)");
-        return isValid;
-    });
- });
+//$(document).ready(function(){
+//    $("#submitBtn").click(function(){
+//        var isValid = true;
+//        $(".form-control").each(function() {
+//            if ( $(this).val() === '' )
+//                isValid = false;
+//        });
+//        if (!isValid)
+//            alert("One or more input fields have been left empty. Please make sure to fill all of them :)");
+//        return isValid;
+//    });
+// });
 
+
+//loads the stops into a dropdown
 $(document).ready(function(){
-    $("#submitBtn").click(function(){
-        $.get("routes", function(data, status){
-//        console.log(data);
+    $.get("stops", function(data, status){
+        var options = $('select[name="startStop"]')
+        $.each(data, function() {
+            options.append($("<option />").val(this.id).text(this.stop_id + "-" + this.name));
         });
     });
 });
 
+//loads the stops into a dropdown
+$(document).ready(function(){
+    $.get("stops", function(data, status){
+        var options = $('select[name="endStop"]')
+        $.each(data, function() {
+            options.append($("<option />").val(this.id).text(this.stop_id + "-" + this.name));
+        });
+    });
+});
+
+//loads the route_ids into a dropdown
+$(document).ready(function(){
+    $.get("routes", function(data, status){
+        var options = $('select[name="route"]')
+        $.each(data, function() {
+            options.append($("<option />").val(this.id).text(this.route_id));
+        });
+    });
+});
+
+
+//trying to use autocomplete:
+//$(document).ready(function(){
+//    $('#tags').autocomplete({
+//        source: $.get("stops", function(data, status){
+//                    return (data.stop_id);
+//                });
+//    });
+//});
