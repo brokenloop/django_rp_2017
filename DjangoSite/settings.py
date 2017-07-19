b4bde59eb1c9b0c945f704c4f08ee8cb29cbb6c1
Daniel Jordan <daniel.jordan1@ucdconnect.ie>
2017-07-19 16:30:06 +0100

    Added allowed hosts, so that we can deploy to server

--- DjangoSite/settings.py
+++ DjangoSite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i=-c72#9cgmbyvqswzj#lwzzp$*gi-=2613s566_$t$%%%5!8d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
